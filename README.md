67c401ed5e88b215dfaa47ed43500a1a410ca7e3
Jorge Segura <jorge.a.segura01@gmail.com>
2023-12-07 16:41:14 -0600

    final notebook started

--- README.md
+++ README.md
@@ -12,3 +12,14 @@ Analyze the impact of aging and composition on concrete's durability.
 This research aims to contribute to the construction industry by enhancing the understanding of concrete behavior, promoting the use of sustainable materials, and aiding in the design of longer-lasting structures. The findings can be instrumental in reducing the environmental impact and improving the safety and economics of construction practices.
 
 Dataset Source: Kaggle - Cement Manufacturing Concrete Dataset
+
+README
+Your README should contain all of the following elements:
+
+Title Gives the name of your project
+Project Description Describes what your project is and why it is important
+Project Goal Clearly states what your project sets out to do and how the information gained can be applied to the real world
+Initial Hypotheses Initial questions used to focus your project
+Project Plan Guides the reader through the different stages of the pipeline as they relate to your project
+Data Dictionary Gives a definition for each of the features used in your report and the units they are measured in, if applicable
+Steps to Reproduce Gives instructions for reproducing your work. i.e. Running your notebook on someone else's computer.
--- explore.py
+++ explore.py
@@ -18,7 +18,8 @@ from sklearn.feature_selection import SelectKBest, RFE, f_regression, Sequential
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
 from sklearn.preprocessing import PolynomialFeatures
-
+from sklearn.preprocessing import MinMaxScaler
+import scipy.stats as stats
 from scipy.stats import pearsonr, spearmanr
 from scipy.stats import shapiro
 
@@ -27,6 +28,9 @@ warnings.filterwarnings("ignore")
 import wrangle as w
 import os
 directory = os.getcwd()
+
+
+
 
 
 def split_data(df):
@@ -50,14 +54,101 @@ def split_data(df):
     
     return train, validate, test
 
-def eval_dist(r, p, α=0.05):
-    if p > α:
-        return print(f"""The data is normally distributed""")
-    else:
-        return print(f"""The data is NOT normally distributed""")
+def preprocess_concrete(df):
+    '''
+    preprocess_concrete will take in values in form of a single pandas dataframe
+    and make the data ready for spatial modeling,
+    including:
+     - splitting the data
+     - scaling continuous columns (excluding the target column 'strength')
+
+    return: three pandas dataframes, ready for modeling structures.
+    '''
+    # split data:
+    train, validate, test = split_data(df)
+
+    # Separate target column 'strength' from features
+    train_features = train.drop(columns='strength')
+    validate_features = validate.drop(columns='strength')
+    test_features = test.drop(columns='strength')
+    train_target = train[['strength']]
+    validate_target = validate[['strength']]
+    test_target = test[['strength']]
+
+    # scale continuous features (excluding 'strength'):
+    scaler = MinMaxScaler()
+    train_features_scaled = pd.DataFrame(
+        scaler.fit_transform(train_features),
+        index=train_features.index,
+        columns=train_features.columns)
+    validate_features_scaled = pd.DataFrame(
+        scaler.transform(validate_features),
+        index=validate_features.index,
+        columns=validate_features.columns)
+    test_features_scaled = pd.DataFrame(
+        scaler.transform(test_features),
+        index=test_features.index,
+        columns=test_features.columns)
+
+    # Recombine scaled features with the target column 'strength'
+    train_scaled = pd.concat([train_features_scaled, train_target], axis=1)
+    validate_scaled = pd.concat([validate_features_scaled, validate_target], axis=1)
+    test_scaled = pd.concat([test_features_scaled, test_target], axis=1)
+
+    return train_scaled, validate_scaled, test_scaled
+
+
+def scale_data(train, 
+               validate, 
+               test, 
+               to_scale):
+    #make copies for scaling
+    train_scaled = train.copy()
+    validate_scaled = test.copy()
+    test_scaled = test.copy()
+
+    #make the thing
+    scaler = MinMaxScaler()
+
+    #fit the thing
+    scaler.fit(train[to_scale])
+
+    #use the thing
+    train_scaled[to_scale] = scaler.transform(train[to_scale])
+    validate_scaled[to_scale] = scaler.transform(validate[to_scale])
+    test_scaled[to_scale] = scaler.transform(test[to_scale])
     
-    
-    
+    return train_scaled, validate_scaled, test_scaled
+
+def check_normality(data, column_name):
+    # Graphical Method - Histogram
+    plt.hist(data[column_name], bins=20)
+    plt.title(f'Histogram of {column_name}')
+    plt.show()
+
+    # Graphical Method - Q-Q Plot
+    stats.probplot(data[column_name], dist="norm", plot=plt)
+    plt.title(f'Q-Q Plot of {column_name}')
+    plt.show()
+
+    # Statistical Test - Shapiro-Wilk Test
+    shapiro_test = stats.shapiro(data[column_name])
+    print(f'Shapiro-Wilk Test for {column_name}: Statistic={shapiro_test.statistic:.3f}, p-value={shapiro_test.pvalue:.3f}')
+
+    # Statistical Test - D’Agostino’s K^2 Test
+    k2, p = stats.normaltest(data[column_name])
+    print(f'D’Agostino’s K^2 Test for {column_name}: Statistic={k2:.3f}, p-value={p:.3f}')
+
+    # Statistical Test - Anderson-Darling Test
+    anderson_test = stats.anderson(data[column_name], dist='norm')
+    print(f'Anderson-Darling Test for {column_name}: Statistic={anderson_test.statistic:.3f}')
+    for i in range(len(anderson_test.critical_values)):
+        sl, cv = anderson_test.significance_level[i], anderson_test.critical_values[i]
+        if anderson_test.statistic < cv:
+            print(f'{sl:.3f}: {cv:.3f}, data looks normal (fail to reject H0)')
+        else:
+            print(f'{sl:.3f}: {cv:.3f}, data does not look normal (reject H0)')
+ 
     
 def eval_Spearman(r, p, α=0.05):
     if p < α:
@@ -69,18 +160,7 @@ P-value: {p}""")
 Spearman’s r: {r:2f}
 P-value: {p}""")
 
-    
-    
-def eval_Pearson(r, p, α=0.05):
-    if p < α:
-        return print(f"""We reject H₀, there is a linear relationship with a Correlation Coefficient of {r:2f}.
-P-value: {p}""")
-    else:
-        return print(f"""We fail to reject H₀: that there is a linear relationship.
-Pearson’s r: {r:2f}
-P-value: {p}""")
-    
-    
+
 def evaluate_reg(y, yhat):
     '''
     based on two series, y_act, y_pred, (y, yhat), we
@@ -92,3 +172,21 @@ def evaluate_reg(y, yhat):
     rmse = mean_squared_error(y, yhat, squared=False)
     r2 = r2_score(y, yhat)
     return rmse, r2
+
+def plot_all_histograms(df, individual_fig_size=(8, 4)):
+    """
+    Plot histograms for each numerical column in the dataframe.
+    
+    Parameters:
+    df (DataFrame): The dataframe containing the data.
+    individual_fig_size (tuple): Figure size for individual histograms.
+    """
+    numeric_cols = df.select_dtypes(include=['number']).columns  # This will select only numeric columns
+
+    for col in numeric_cols:
+        plt.figure(figsize=individual_fig_size)
+        plt.title(col.upper())  # Convert title to uppercase
+        df[col].hist(bins=50)
+        plt.xticks(rotation=30)  # Rotate x-axis labels by 30 degrees
+        plt.grid(False)  # Hide the grid
+        plt.show()  # Show each plot individually
